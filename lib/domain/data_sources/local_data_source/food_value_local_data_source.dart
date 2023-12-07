f88bf4cb7a87bfe7e671f10aaa135ca4ac68a0a3
admin <admin@VCBMacAir.local>
2023-12-07 10:43:13 +0700

    update

--- lib/domain/data_sources/local_data_source/food_value_local_data_source.dart
+++ lib/domain/data_sources/local_data_source/food_value_local_data_source.dart
@@ -1,6 +1,5 @@
+import 'package:web_provise/core/http_client/list_response.dart';
 import 'package:web_provise/data/models/food_value_model.dart';
-
-import '../../../core/http_client/list_response.dart';
 
 abstract class FoodValueLocalDataSource {
   Future<ListResponse<FoodValueModel>> getListFoodValue();
